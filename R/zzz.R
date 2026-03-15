dd9b0f50976397fcce3a89e9488263cd1aadbd79
Daniel de la Rosa @ BlackBox <drdsdaniel@gmail.com>
2026-03-14 21:12:37 -0400

    fix: update cache path retrieval in .onLoad and .onAttach functions for consistency

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@
 .pkg_env <- new.env(parent = emptyenv())
 
 .onLoad <- function(libname, pkgname) {
-  cache_path <- fs::path(fs::path_home_r(), ".geodom")
+  cache_path <- getOption("geodom.cache.path", default = fs::path(fs::path_home_r(), ".geodom"))
   fs::dir_create(cache_path)
 
   # 2. Guardar el "board" de cach\u00e9 local en el entorno del paquete.
@@ -14,12 +14,16 @@
   # 3. Guardar la URL base para todas las descargas.
   .pkg_env$base_data_url <- "https://geodom-worker.drdsdaniel.workers.dev/"
 
-  if (is.null(options("geodom.cache.path"))) {
+  options(geodom.cache.path = cache_path)
+}
+
+.onAttach <- function(libname, pkgname) {
+  cache_path <- getOption("geodom.cache.path")
+  if (!is.null(cache_path)) {
     packageStartupMessage(
       "GeoDOM: Usando cach\u00e9 local en '",
       cache_path,
       "'"
     )
-    options(geodom.cache.path = cache_path)
   }
 }
